b07905e0caaab912414c40d14505a4e66a13f829
home <kristianhnielsen@protonmail.com>
2022-04-02 14:24:41 +0800

    clientside router fix test

--- src/Navbar/Desktop/Navigation.js
+++ src/Navbar/Desktop/Navigation.js
@@ -1,13 +1,17 @@
-import { Link } from "react-router-dom";
 
 const Navigation = () => {
     return (  
         <nav className="navbar">
-            <Link to="/">HJEM</Link>
+            <a href="/">HJEM</a>
+            <a href="/radgivning">RÅDGIVNING</a>
+            <a href="/pris">PRIS</a>
+            <a href="/about">OM</a>
+            <a href="/contact">KONTAKT</a>
+            {/* <Link to="/">HJEM</Link>
             <Link to="/radgivning">RÅDGIVNING</Link>
             <Link to="/pris">PRIS</Link>
             <Link to="/about">OM</Link>
-            <Link to="/contact">KONTAKT</Link>
+            <Link to="/contact">KONTAKT</Link> */}
         </nav>
     );
 }
