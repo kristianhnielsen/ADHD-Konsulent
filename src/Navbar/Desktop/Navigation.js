74e7ef9dc775cfd216c25374283344fa7cbe35f2
home <kristianhnielsen@protonmail.com>
2022-04-02 15:56:01 +0800

    clientside render fix undo

--- src/Navbar/Desktop/Navigation.js
+++ src/Navbar/Desktop/Navigation.js
@@ -1,11 +1,13 @@
+import { Link } from "react-router-dom"
+ 
 const Navigation = () => {
     return (  
         <nav className="navbar">
-            <a href="/">HJEM</a>
-            <a href="/radgivning">RÅDGIVNING</a>
-            <a href="/pris">PRIS</a>
-            <a href="/about">OM</a>
-            <a href="/contact">KONTAKT</a>
+            <Link to="/">HJEM</Link>
+            <Link to="/radgivning">RÅDGIVNING</Link>
+            <Link to="/pris">PRIS</Link>
+            <Link to="/about">OM</Link>
+            <Link to="/contact">KONTAKT</Link>
         </nav>
     );
 }
--- src/Navbar/Mobile/Navigation.js
+++ src/Navbar/Mobile/Navigation.js
@@ -1,11 +1,13 @@
+import { Link } from "react-router-dom"
+
 const Navigation = ({closeMenu}) => {
     return (
         <nav className="navbar">
-            <a href="/" onClick={() => closeMenu()}>HJEM</a>
-            <a href="/radgivning" onClick={() => closeMenu()}>RÅDGIVNING</a>
-            <a href="/pris" onClick={() => closeMenu()}>PRIS</a>
-            <a href="/about" onClick={() => closeMenu()}>OM</a>
-            <a href="/contact" onClick={() => closeMenu()}>KONTAKT</a>
+            <Link to="/" onClick={() => closeMenu()}>HJEM</Link>
+            <Link to="/radgivning" onClick={() => closeMenu()}>RÅDGIVNING</Link>
+            <Link to="/pris" onClick={() => closeMenu()}>PRIS</Link>
+            <Link to="/about" onClick={() => closeMenu()}>OM</Link>
+            <Link to="/contact" onClick={() => closeMenu()}>KONTAKT</Link>
         </nav>
     );
 }
